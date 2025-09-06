b6612fcd979b91f12dcfef0e8f685068e5076a17
Ryan Yeske <ryan@ryanyeske.com>
2025-09-06 12:59:55 -0700

    Add command help

--- cli/cli.go
+++ cli/cli.go
@@ -1,11 +1,11 @@
 package cli
 
 var CLI struct {
-	Version VersionCmd `cmd:""`
+	Version VersionCmd `cmd:"" help:"show the build version"`
 	Notes   NotesCmd   `cmd:""`
-	Events  EventsCmd  `cmd:""`
-	Ddate   DDateCmd   `cmd:""`
-	Debug   DebugCmd   `cmd:""`
-	Serve   ServeCmd   `cmd:""`
-	Bug     BugCmd     `cmd:""`
+	Events  EventsCmd  `cmd:"" help:"dump the event log"`
+	Ddate   DDateCmd   `cmd:"" help:"show current discordian date"`
+	Serve   ServeCmd   `cmd:"" help:"start a webserver"`
+	Bug     BugCmd     `cmd:"" help:"report a bug"`
+	Debug   DebugCmd   `cmd:"" help:"debug commands"`
 }
