bc7abd38591655ff3fe6fd4c75879ba3fd50b499
Ryan Yeske <ryan@ryanyeske.com>
2025-09-06 11:28:48 -0700

    Remove reminders

--- cli/cli.go
+++ cli/cli.go
@@ -1,15 +1,10 @@
 package cli
 
-import (
-	"github.com/rcy/whatever/cli/reminders"
-)
-
 var CLI struct {
-	Version   VersionCmd    `cmd:""`
-	Notes     NotesCmd      `cmd:""`
-	Reminders reminders.Cmd `cmd:""`
-	Events    EventsCmd     `cmd:""`
-	Ddate     DDateCmd      `cmd:""`
-	Debug     DebugCmd      `cmd:""`
-	Serve     ServeCmd      `cmd:""`
+	Version VersionCmd `cmd:""`
+	Notes   NotesCmd   `cmd:""`
+	Events  EventsCmd  `cmd:""`
+	Ddate   DDateCmd   `cmd:""`
+	Debug   DebugCmd   `cmd:""`
+	Serve   ServeCmd   `cmd:""`
 }
